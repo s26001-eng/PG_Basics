43cd6b1adc7e2b3872d3e2b200884168e93e5781
s26001-eng <s26001@std.it-college.ac.jp>
2026-05-26 07:56:39 +0000

    例題13まで

---
+++ Chapter03/ex3-09.py
@@ -0,0 +1,7 @@
+hi = "Hello,World!"
+my_float = 2.2
+my_boolean = True
+
+print(hi)
+print(my_float)
+print(my_boolean)
---
+++ Chapter03/ex3-10.py
@@ -0,0 +1,3 @@
+#＾の閉じ忘れエラーを発生させる
+my_string = "Hello World.
+print(my_string)
